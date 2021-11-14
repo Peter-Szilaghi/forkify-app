97e6f36b4020f80283ee750e4c58b21031b53054
Peter-Szilaghi <szilaghipeteri@gmail.com>
2021-11-14 11:00:23 +0200

    remove log welcome

--- src/js/controller.js
+++ src/js/controller.js
@@ -130,10 +130,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarkesView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -142,7 +138,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
 
